5c941489f96367fadc8b8250d812b557be9b47d7
twolak88 <twolak88@gmail.com>
2020-09-11 19:29:39 +0200

    spring mongo db integration test fix

--- src/test/java/com/twolak/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/twolak/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -4,27 +4,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.twolak.springframework.bootstrap.DataLoader;
 import com.twolak.springframework.domain.UnitOfMeasure;
 
-@Disabled
 @ExtendWith(SpringExtension.class)
-//@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 	
 	@Autowired
 	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	@Autowired
+	private  CategoryRepository categoryRepository;
+	
+	@Autowired
+	private  RecipeRepository recipeRepository;
 
 	@BeforeEach
 	public void setUp() throws Exception {
-
+		DataLoader dataLoader = new DataLoader(unitOfMeasureRepository, categoryRepository, recipeRepository);
+		
+		dataLoader.onApplicationEvent(null);
+	}
+	
+	@AfterEach
+	private void clean() {
+		if (unitOfMeasureRepository.count() > 0) {
+			unitOfMeasureRepository.deleteAll();
+		}
+		if (categoryRepository.count() > 0) {
+			categoryRepository.deleteAll();
+		}
+		if (recipeRepository.count() > 0) {
+			recipeRepository.deleteAll();
+		}
 	}
 	
 	@Test
